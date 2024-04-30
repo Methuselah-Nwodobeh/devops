d1d681d0b9142c56dfae02a2d6c67551db2bf16a
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2024-04-30 11:28:32 +0000

    Initial commit

---
+++ src/main/java/com/amalitech/upskilling/App.java
@@ -0,0 +1,13 @@
+package com.amalitech.upskilling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
